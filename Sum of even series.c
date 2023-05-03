1eb8683c31262e46d36e7337ac91e9b70323c1bd
AkashB005 <123925241+AkashB005@users.noreply.github.com>
2023-05-03 11:22:59 +0530

    Update Sum of even series.c

--- Sum of even series.c
+++ Sum of even series.c
@@ -4,13 +4,11 @@ int main()
 {
     int i, n, sum=0;
 
-    /* Input upper limit from user */
     printf("Enter upper limit: ");
     scanf("%d", &n);
 
     for(i=2; i<=n; i+=2)
     {
-        /* Add current even number to sum */
         sum += i;
     }
 
